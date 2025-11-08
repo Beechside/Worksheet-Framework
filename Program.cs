e4d173c97cb65bd4348b820a66309e2e4b0bc813
Peter Morris <p.morris@worc.ac.uk>
2025-11-08 08:54:32 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Worksheet_Framework;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Worksheet worksheet = new();
+        worksheet.Exercise1();
+        worksheet.Exercise2();
+    }
+}
+
+internal class Worksheet
+{
+    public void Exercise1()
+    { 
+    }
+
+    public void Exercise2()
+    {
+    }
+}
